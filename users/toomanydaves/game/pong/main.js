ee55c51d02abe1cc8bbb6ee980b6c2be0d92081a
Sean Jacke <sean.jacke@learnship.com>
2024-06-23 15:19:50 +0100

    Add multiple levels

--- users/toomanydaves/game/pong/main.js
+++ users/toomanydaves/game/pong/main.js
@@ -1,44 +1,78 @@
 const canvas = document.getElementById('pong');
 const ctx = canvas.getContext('2d');
-const ballRadius = 10;
-const ballSpeed = 4;
-const paddleHeight = 10;
-const paddleWidth = 75;
-const brickRowCount = 3;
-const brickColumnCount = 5;
-const brickWidth = 75;
-const brickHeight = 20
-const brickPadding = 10;
-const brickOffsetTop = 30;
-const brickOffsetLeft = 30;
+
+const levelSettings = [
+    {
+        ballRadius: 10,
+        ballSpeed: 4,
+        paddleHeight: 10,
+        paddleWidth: 80,
+        brickRowCount: 3,
+        brickColumnCount: 5,
+        brickWidth: 75,
+        brickHeight: 20,
+        brickPadding: 10,
+        brickOffsetTop: 30,
+        brickOffsetLeft: 30
+    }, {
+        ballRadius: 8,
+        ballSpeed: 5,
+        paddleHeight: 10,
+        paddleWidth: 95,
+        brickRowCount: 5,
+        brickColumnCount: 5,
+        brickWidth: 75,
+        brickHeight: 20,
+        brickPadding: 10,
+        brickOffsetTop: 40,
+        brickOffsetLeft: 30
+    }
+]
+
 const bricks = [];
 
 let animationFrameId;
+
 let lives = 3;
+let level = 0;
 let score = 0;
+let settings;
+let bricksRemaining;
 
 //ball coodinates and motion
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = ballSpeed;
-let dy = -ballSpeed;
+let x;
+let y;
+let dx;
+let dy;
 
 //paddle placement
-let paddleX = (canvas.width - paddleWidth) / 2;
-let rightPressed = false;
-let leftPressed = false;
+let paddleX;
+let rightPressed;
+let leftPressed;
 
-//bricks array
-for (let c = 0; c < brickColumnCount; c++) {
-    bricks[c] = [];
-    for (let r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0, status: 1 };
+function loadLevel () {
+    settings = levelSettings[level];
+    x = canvas.width / 2;
+    y = canvas.height - 30;
+    dx = settings.ballSpeed;
+    dy = -settings.ballSpeed;
+    paddleX = (canvas.width - settings.paddleWidth) / 2;
+    rightPressed = false;
+    leftPressed = false;
+
+    for (let c = 0; c < settings.brickColumnCount; c++) {
+        bricks[c] = [];
+        for (let r = 0; r < settings.brickRowCount; r++) {
+            bricks[c][r] = { x: 0, y: 0, status: 1 };
+        }
     }
+
+    bricksRemaining = settings.brickColumnCount * settings.brickRowCount;
 }
 
 function drawBall () {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.arc(x, y, settings.ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -48,9 +82,9 @@ function drawPaddle () {
     ctx.beginPath();
     ctx.rect(
         paddleX,
-        canvas.height - paddleHeight,
-        paddleWidth,
-        paddleHeight
+        canvas.height - settings.paddleHeight,
+        settings.paddleWidth,
+        settings.paddleHeight
     );
     ctx.fillStyle = "#0095DD";
     ctx.fill();
@@ -58,17 +92,17 @@ function drawPaddle () {
 }
 
 function drawBricks () {
-    for (let c = 0; c < brickColumnCount; c++) {
-        for (let r = 0; r < brickRowCount; r++) {
+    for (let c = 0; c < settings.brickColumnCount; c++) {
+        for (let r = 0; r < settings.brickRowCount; r++) {
             if (bricks[c][r].status === 1) {
-                const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-                const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                const brickX = c * (settings.brickWidth + settings.brickPadding) + settings.brickOffsetLeft;
+                const brickY = r * (settings.brickHeight + settings.brickPadding) + settings.brickOffsetTop;
 
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
 
                 ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.rect(brickX, brickY, settings.brickWidth, settings.brickHeight);
                 ctx.fillStyle = '#0095DD';
                 ctx.fill();
                 ctx.closePath();
@@ -109,23 +143,30 @@ function followMouse (e) {
     const relativeX = e.clientX - canvas.offsetLeft;
 
     if (relativeX > 0 && relativeX < canvas.width) {
-        paddleX = relativeX - paddleWidth / 2;
+        paddleX = relativeX - settings.paddleWidth / 2;
     }
 }
 
 function detectCollisionWithBricks () {
-    for (let c = 0; c < brickColumnCount; c++) {
-        for (let r = 0; r < brickRowCount; r++) {
+    for (let c = 0; c < settings.brickColumnCount; c++) {
+        for (let r = 0; r < settings.brickRowCount; r++) {
             const b = bricks[c][r];
 
             if (b.status === 1) {
-                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                if (x > b.x && x < b.x + settings.brickWidth && y > b.y && y < b.y + settings.brickHeight) {
                     dy = -dy;
                     b.status = 0;
                     score++;
-                    if (score === brickRowCount * brickColumnCount) {
-                        alert('Congratulations, you won!! Play again?');
-                        document.location.reload();
+                    bricksRemaining--;
+                    if (!bricksRemaining) {
+                        if (level == levelSettings.length - 1) {
+                            alert('Congratulations, you won!! Play again?');
+                            document.location.reload();
+                        } else {
+                            level++;
+                            alert('Well done! Ready for the next level?');
+                            loadLevel();
+                        }
                     }
                 }
             }
@@ -142,19 +183,19 @@ function draw () {
 
     // shift paddle without leaving canvas
     if (rightPressed) {
-        paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+        paddleX = Math.min(paddleX + 7, canvas.width - settings.paddleWidth);
     } else if (leftPressed) {
         paddleX = Math.max(paddleX - 7, 0);
     }
 
     //collision detection
-    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+    if (x + dx < settings.ballRadius || x + dx > canvas.width - settings.ballRadius) {
         dx = -dx;
     }
-    if (y + dy < ballRadius) {
+    if (y + dy < settings.ballRadius) {
         dy = -dy;
-    } else if (y + dy > canvas.height - ballRadius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
+    } else if (y + dy > canvas.height - settings.ballRadius) {
+        if (x > paddleX && x < paddleX + settings.paddleWidth) {
             dy = -dy
         } else {
             lives--;
@@ -165,9 +206,9 @@ function draw () {
                 alert(`1 life down, ${lives} left.`);
                 x = canvas.width / 2;
                 y = canvas.height - 30;
-                dx = ballSpeed;
-                dy = -ballSpeed;
-                paddleX = (canvas.width - paddleWidth) / 2;
+                dx = settings.ballSpeed;
+                dy = -settings.ballSpeed;
+                paddleX = (canvas.width - settings.paddleWidth) / 2;
             }
         }
     }
@@ -183,6 +224,7 @@ function draw () {
 }
 
 function startGame () {
+    loadLevel();
     draw();
     document.addEventListener('keydown', movePaddle, false);
     document.addEventListener('keyup', stopPaddle, false);
