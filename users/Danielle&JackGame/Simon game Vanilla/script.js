12250a3cf7115d123d43923f300bfd3681fb44bf
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-06-25 19:21:06 +0100

    basic game functionality

--- users/Danielle&JackGame/Simon game Vanilla/script.js
+++ users/Danielle&JackGame/Simon game Vanilla/script.js
@@ -4,7 +4,7 @@ let gamePattern = [];
 
 let userPattern = [];
 
-let timesClicked = [];
+let timesClicked = 0;
 
 let gameLevel = 1;
 
@@ -17,12 +17,14 @@ const redAudio = document.getElementById("red_button_sound");
 const greenAudio = document.getElementById("green_button_sound");
 const blueAudio = document.getElementById("blue_button_sound");
 const yellowAudio = document.getElementById("yellow_button_sound");
+const levelCounter = document.getElementById("level-counter");
 
 // Resets all the game arrays
 function resetGame() {
   gamePattern = [];
   userPattern = [];
   timesClicked = [];
+  gameLevel = 1;
 }
 
 // Generates random number out of 4 (as there are 4 colours)
@@ -34,7 +36,6 @@ function randomNumber() {
 function pushToGameArray() {
   const myRandomNumber = randomNumber();
   gamePattern.push(buttonColours[myRandomNumber]);
-  // console.log(gamePattern);
 }
 
 let i = 0;
@@ -71,7 +72,7 @@ function playLoop() {
     i++; //  increment the counter
     if (i <= gameLevel) {
       playLoop();
-    }
+    } else i = 0;
   }, 1000);
 }
 
@@ -79,69 +80,60 @@ function playLoop() {
 // Obviously will start with one, then two, then three, onwards colours. I started with four to
 // get the game logic going
 // Need to get a timeout going on this so that it selects them slowly rather than all at once
-function startGame() {
-  resetGame();
-  for (let i = 1; i <= gameLevel; i++) {
-    pushToGameArray();
-    console.log(i);
-    //  const myTimeout = setTimeout(pushToGameArray, 1000);
-  }
+function startRound() {
+  pushToGameArray();
+  levelCounter.innerHTML = gameLevel;
   playLoop();
-  console.log(gamePattern);
 }
 
 // Checks the elements of the gamePattern and the userPattern, then
 // console logs LOSER until they match, then when they match console logs WINNER
 function checkWin() {
   let consistencyCounter = 0;
-  console.log(userPattern);
   for (let i = 0; i < gamePattern.length; i++) {
     if (gamePattern[i] === userPattern[i]) {
       consistencyCounter++;
-      console.log(consistencyCounter);
     }
   }
   if (consistencyCounter === gameLevel && timesClicked === gameLevel) {
     console.log("WINNER");
     gameLevel++;
-    startGame();
+    userPattern = [];
+    timesClicked = 0;
+    startRound();
   } else if (timesClicked === gameLevel) {
     console.log("LOSER");
   }
 }
 
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startRound);
 
 // Pushes "red" to the userPattern, adds 1 to timesClicked, and checks for a win
 redButton.addEventListener("click", function () {
-  //   console.log("red button pressed");
   userPattern.push("red");
   timesClicked++;
-  checkWin();
   redAudio.play();
+  checkWin();
 });
 
 // Pushes "green" to the userPattern, adds 1 to timesClicked, and checks for a win
 greenButton.addEventListener("click", function () {
-  //   console.log("green button pressed");
   userPattern.push("green");
   timesClicked++;
-  checkWin();
   greenAudio.play();
+  checkWin();
 });
 
 // Pushes "blue" to the userPattern, adds 1 to timesClicked, and checks for a win
 blueButton.addEventListener("click", function () {
-  //   console.log("blue button pressed");
   userPattern.push("blue");
   timesClicked++;
-  checkWin();
   blueAudio.play();
+  checkWin();
 });
 
 // Pushes "green" to the userPattern, adds 1 to timesClicked, and checks for a win
 yellowButton.addEventListener("click", function () {
-  //   console.log("yellow button pressed");
   userPattern.push("yellow");
   timesClicked++;
   checkWin();
