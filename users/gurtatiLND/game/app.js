b1cd7a14bc759287c5e905a3c611122bab840966
GurTati <tatiana.gurova.j@gmail.com>
2024-06-26 14:06:41 +0100

    initial set up for RPS-game

---
+++ users/gurtatiLND/game/app.js
@@ -0,0 +1,15 @@
+const gameLogic =() => {
+    let player = 0;
+    let computer = 0;
+    let movesCounter = 0;
+}
+
+const playGame = () => {
+    const paper = document.getElementById('paper');
+    const rock = document.getElementById('rock');
+    const scissors = document.getElementById('scissors');
+
+    const arrOfPlayerChoice = [paper, rock, scissors];
+    //create an array for the computer options
+    const arrOfComputerChoice = [];
+}
