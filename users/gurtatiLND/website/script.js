f2cc98f4832b59780e8ea104d4659084262da644
GurTati <tatiana.gurova.j@gmail.com>
2024-07-09 11:51:25 +0100

    trying to change the modal box appearance

--- users/gurtatiLND/website/script.js
+++ users/gurtatiLND/website/script.js
@@ -13,48 +13,48 @@ function validateForm() {
     messageError.textContent = "";
 
     //get the modal box
-    const modal = document.getElementById("myModal");
-    const btn = document.getElementById("modalBtn");
-    const span = document.getElementsByClassName("close")[0];
-
+    const modal = document.getElementsByClassName("modal");
     const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     let isValid = true;
 
     if (nameInput === "" || /\d/.test(nameInput)) {
         nameError.textContent = "Please enter your name properly";
+        nameError.classList.add('error');
         isValid = false;
     }
 
     if (emailInput === "" || emailRegExp.test(emailInput.value)) {
         emailError.textContent = "Please enter a valid email address";
+        emailError.classList.add('error');
         isValid = false;
     }
 
     if (messageInput === "" || /\d/.test(messageInput)) {
         messageError.textContent = "Please enter your message";
+        messageError.classList.add('error');
         isValid = false;
     }
     
     if (isValid) {       
        // When the user clicks the button, open the modal 
-       btn.onclick = function() {
         modal.style.display = "block";
-       }
-       // When the user clicks on <span> (x), close the modal
-       span.onclick = function() {
+        
+    }
+    // When the user clicks on <span> (x), close the modal
+    const span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
         modal.style.display = "none";
-       }
+    }
   
-       // When the user clicks anywhere outside of the modal, close it
-       window.onclick = function(event) {
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
         if (event.target == modal) {
-           modal.style.display = "none";
+            modal.style.display = "none";
         }
-       };
     }
 
-    return isValid;
+    return isValid; // Prevent default form submission for demonstration purposes
 
 };
 
