034da165d5096c999ba52d3e1a123fbbe7c5c941
GurTati <tatiana.gurova.j@gmail.com>
2024-07-10 14:23:58 +0100

    Refactored error handling and validation for the modal box

--- users/gurtatiLND/game/app.js
+++ users/gurtatiLND/game/app.js
@@ -1,22 +1,11 @@
 //that will contain all the logic of the game
 const gameLogic =() => {
-    let player = 0;
-    let computer = 0;
-    let movesCounter = 0; //moves which will keep the record of the player’s score, computer’s score, and moves completed respectively.
+  const choices = ['paper', 'rock', 'scissors'];
 
+  const computerChoices = choices[Math.floor(Math.random() * 3)];
+  
 
-  const playGame = () => {
-      const paper = document.getElementById('paper');
-      const rock = document.getElementById('rock');
-      const scissors = document.getElementById('scissors');
-
-      const arrOfPlayerChoice = [paper, rock, scissors];
-      //create an array for the computer options
-      const arrOfComputerChoice = ['paper', 'rock', 'scissors'];
-
-      arrOfPlayerChoice.forEach((element) => 
-        element.addEventListener('click', function() {
-            
-        }))
-  }
+  const paper = document.getElementById('paper');
+  const rock = document.getElementById('rock');
+  const scissors = document.getElementById('scissors');
 }
