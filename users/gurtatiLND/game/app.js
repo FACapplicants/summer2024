3ef2a08ebf5fd97a389e577b28eaa0b3e130c516
GurTati <tatiana.gurova.j@gmail.com>
2024-07-17 15:02:52 +0100

    implemeted game mechanics

--- users/gurtatiLND/game/app.js
+++ users/gurtatiLND/game/app.js
@@ -1,11 +1,44 @@
+const choices = ['paper', 'rock', 'scissors'];
+const playerScore = document.getElementById('playerScore');
+const computerScore = document.getElementById('computerScore');
+const result = document.getElementById('result');
+const loseAudio = document.querySelector('#audio-losing');
+const winAudio = document.querySelector('#audio-winning');
+
 //that will contain all the logic of the game
-const gameLogic =() => {
-  const choices = ['paper', 'rock', 'scissors'];
+const gameLogic =(playerChoice) => {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let outcome='';
 
-  const computerChoices = choices[Math.floor(Math.random() * 3)];
-  
+  if (playerChoice === computerChoice) {
+    outcome = "IT'S A TIE";
+  } else {
+    switch(playerChoice) {
+        case "rock":
+          outcome = (computerChoice === 'scissors') ? 'YOU WIN!' : "YOU LOSE!";
+          break;
+        case "paper":
+          outcome = (computerChoice === 'rock') ? 'YOU WIN!' : 'YOU LOSE!';
+          break;
+        case 'scissors':
+          outcome = (computerChoice === 'paper') ? 'YOU WIN!' : "YOU LOSE!";
+          break;
+    }
+  }
+  playerScore.textContent = `PLAYER: ${playerChoice}`;
+  computerScore.textContent = `COMPUTER: ${computerChoice}`;
+  result.textContent = outcome;
 
-  const paper = document.getElementById('paper');
-  const rock = document.getElementById('rock');
-  const scissors = document.getElementById('scissors');
+  result.classList.remove('bold', 'italic');
+
+  switch(outcome) {
+      case 'YOU WIN!':
+        result.classList.add('bold');
+        winAudio.play();
+        break;
+      case "YOU LOSE!":
+        result.classList.add('italic');
+        loseAudio.play();
+        break;
+  }
 }
