d92b4f2621ad8c822bfed9f3bace6c7e834ad0a8
N2SuccessAll <mahamadtoumani@gmail.com>
2024-07-20 16:51:16 +0100

    Adding build the website and files (index.html, styles.css, and script.js).

---
+++ users/konomissira/website/js/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
