4754bf06208ec07761b09764bfb15acfaa0ff0e5
Daya <thay.diana@gmail.com>
2024-08-06 07:50:37 +0100

    .

---
+++ users/Daya/JavaScript/script.js
@@ -0,0 +1,41 @@
+// console.log("Hello World"); 
+
+/* ====================== function syntax and calling the function to activate it =========================
+    function helloWorld() {
+    console.log("Hello World.");
+    }
+    helloWorld(); // this will call the function and will print Hello World. to the console
+*/
+
+/* ====================== function syntax and calling the function to activate it =========================
+
+*/
+
+
+/* ====================== passing an argument (firstStatement, secondStatement) =========================
+
+    function helloWorld(firstStatement, secondStatement) {
+    console.log(firstStatement);
+    console.log(secondStatement);
+}
+    helloWorld(1, "Hello Daya!");
+*/
+
+/* ====================== passing an argument (firstStatement, secondStatement) =========================
+    let firstStatement = "chimney";
+
+    function helloWorld(firstStatement, secondStatement) {
+    console.log(firstStatement);
+    console.log(secondStatement);
+}
+    helloWorld(1, "Hello Daya!"); // this will print 1 and "Hello Daya!" because the variable is outside of the function scope
+*/
+
+
+
+
+
+
+
+
+
