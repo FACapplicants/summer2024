9dd23c0e60313bbc5d0b296c2a2e78d0bb9262b4
Danielle Lee <82280304+Danielle73@users.noreply.github.com>
2024-08-06 11:41:03 +0100

    Fix of light-theme images to be present on load

--- users/Danielle73/script.js
+++ users/Danielle73/script.js
@@ -1,12 +1,4 @@
-function lightsOff() {
-    
-    const darkMode = document.body;
-    const isDarkMode = darkMode.classList.toggle("dark-theme");
-
-    const nav = document.querySelector('.navigation');
-    nav.classList.toggle('dark-theme');
-
-    /* JS for image change */
+function setThemeImages(isDarkMode) {
     const changeHeadImage = document.getElementById('introImage');
     const originalSrc = "images/bermuda-113.png";
     const newSrc = "images/bermuda-come-back-later-1.png";
@@ -17,7 +9,6 @@ function lightsOff() {
         changeHeadImage.src = originalSrc;
     }
 
-    // Image change for bg image of text element
     const aboutCard = document.querySelector('.bg-img');
     const originalBg = 'url("images/bermuda-22.png")';
     const newBg = 'url("images/bermuda-114.png")';
@@ -28,7 +19,6 @@ function lightsOff() {
         aboutCard.style.backgroundImage = originalBg;
     }
 
-    // image change for bg image for profile picture
     const aboutMeImg = document.querySelector('.about-me-img');
     const originalAboutMeBg = 'url("images/flamenco-153.png")';
     const newAboutMeBg = 'url("images/flamenco-137.png")';
@@ -36,48 +26,57 @@ function lightsOff() {
     if (isDarkMode) {
         aboutMeImg.style.backgroundImage = newAboutMeBg;
     } else {
-        aboutMeImg.style.backgroundImage = originalAboutMeBg;     
+        aboutMeImg.style.backgroundImage = originalAboutMeBg;
     }
 
-    //image change for contact icons
-
-    //GITHUB
     const githubIcon = document.getElementById("gitIcon");
     const originalgithubIcon = "https://img.icons8.com/material-outlined/36/000000/github.png";
     const newgithubIcon = "https://img.icons8.com/?size=40&id=44900&format=png&color=FFFFFF";
 
     if (isDarkMode) {
-        githubIcon.src =newgithubIcon;
+        githubIcon.src = newgithubIcon;
     } else {
         githubIcon.src = originalgithubIcon;
     }
-
-    //LINKEDIN
 
     const linkedInIcon = document.getElementById("linkedInIcon");
     const originallinkedInIcon = "https://img.icons8.com/ios-glyphs/36/000000/linkedin.png";
     const newLinkedInIcon = "https://img.icons8.com/?size=40&id=8808&format=png&color=FFFFFF";
 
     if (isDarkMode) {
-        linkedInIcon.src =newLinkedInIcon;
+        linkedInIcon.src = newLinkedInIcon;
     } else {
         linkedInIcon.src = originallinkedInIcon;
     }
-
-    //CODEWARS
 
     const CodeWarsIcon = document.getElementById("CodeWarsIcon");
     const originalCodeWarsIcon = "https://img.icons8.com/?size=36&id=PlozTIZdV8xJ&format=png&color=000000";
     const newCodeWarsIcon = "https://img.icons8.com/?size=40&id=PlozTIZdV8xJ&format=png&color=FFFFFF";
 
     if (isDarkMode) {
-        CodeWarsIcon.src =newCodeWarsIcon;
+        CodeWarsIcon.src = newCodeWarsIcon;
     } else {
         CodeWarsIcon.src = originalCodeWarsIcon;
     }
-
 }
 
+function lightsOff() {
+    const darkMode = document.body;
+    const isDarkMode = darkMode.classList.toggle("dark-theme");
+
+    const nav = document.querySelector('.navigation');
+    nav.classList.toggle('dark-theme');
+
+    setThemeImages(isDarkMode);
+}
+
+// Calls the setThemeImages function to set the initial theme & check 
+// This should fix the light theme image issue. 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const isDarkMode = document.body.classList.contains("dark-theme");
+    setThemeImages(isDarkMode);
+});
 
 /* Sticky / Fixed vertical navbar for responsive screens */
 window.onscroll = () => {
@@ -96,4 +95,3 @@ function topScroll() {
         navbar.classList.remove("sticky");
     }
 }
-
